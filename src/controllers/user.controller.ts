714e74a96222bf634d49811a6a3878462fd2cd76
Mazen Othman <112982340+MazenOth@users.noreply.github.com>
2024-09-04 12:05:02 +0300

    feat(user.controller): create deleteUser and getAllUsers route handlers
    - Contains some refact in error handling
    - Fix naming of  updateUser()

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -6,17 +6,41 @@ class UserController {
     try {
       const result = await UserService.createUser(req.body);
       res.status(201).json(result);
-    } catch (error) {
-      res.status(500).json({ error });
+    } catch (error: any) {
+      res.status(500).json({ message: error.message });
     }
   }
 
-  async updateClient(req: Request, res: Response): Promise<void> {
+  async updateUser(req: Request, res: Response): Promise<void> {
     try {
+      const userId = req.params.id;
+      req.body.id = userId;
       const result = await UserService.updateUser(req.body);
       res.status(201).json(result);
-    } catch (error) {
-      res.status(500).json({ error });
+    } catch (error: any) {
+      res.status(500).json({ message: error.message });
+    }
+  }
+
+  async deleteUser(req: Request, res: Response): Promise<void> {
+    try {
+      const deleted = await UserService.deleteUser(parseInt(req.params.id, 10));
+      if (deleted) {
+        res.status(204).json();
+      } else {
+        res.status(404).json({ message: 'User not found' });
+      }
+    } catch (error: any) {
+      res.status(500).json({ message: error.message });
+    }
+  }
+
+  async getAllUsers(req: Request, res: Response): Promise<void> {
+    try {
+      const users = await UserService.getAllUsers(req.body);
+      res.status(200).json(users);
+    } catch (error: any) {
+      res.status(500).json({ message: error.message });
     }
   }
 }
