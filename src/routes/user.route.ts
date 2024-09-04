fab0241d45982301271689862cd2f255dc5fe1f0
Mazen Othman <112982340+MazenOth@users.noreply.github.com>
2024-09-04 12:06:39 +0300

    feat(user.route): create routes for deleteUser and getAllUsers route handlers

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -5,5 +5,7 @@ const router = Router();
 
 router.post('/users/register', UserController.createUser);
 router.put('/users/:id', UserController.updateUser);
+router.delete('/users/:id', UserController.deleteUser);
+router.get('/users', UserController.getAllUsers);
 
 export default router;
