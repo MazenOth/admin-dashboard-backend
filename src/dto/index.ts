3286996fa0ea45e7e09aec22ec993effc387e926
Mazen Othman <112982340+MazenOth@users.noreply.github.com>
2024-09-07 13:47:03 +0300

    feat(dto): exporting new dtos

--- src/dto/index.ts
+++ src/dto/index.ts
@@ -2,3 +2,5 @@ export * from './user/user.dto';
 export * from './user/getUsers.dto';
 export * from './matching/potentialMatch.dto';
 export * from './matching/verifyMatch.dto';
+export * from './matching/pagination.dto';
+export * from './matching/helperClientId.dto';
