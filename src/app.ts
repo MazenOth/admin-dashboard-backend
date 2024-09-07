42ab7c70e62a4191090982e9191d2e1500321ce0
Mazen Othman <112982340+MazenOth@users.noreply.github.com>
2024-09-07 13:45:57 +0300

    feat(app): add cors middleware

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express, { Application } from 'express';
 import { initializeModels } from './models';
-import routes from './routes'; 
+import routes from './routes';
+import cors from "cors";
 
 const app: Application = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.use(routes);
 
